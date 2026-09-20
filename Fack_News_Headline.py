98eca5014d2d67f6c9b634ca7c7799e20ef6c98c
Amisha Gurau <amishagurau9@gmail.com>
2026-09-20 17:58:45 +0545

    simple entertainment Code

---
+++ Fack_News_Headline.py
@@ -0,0 +1,24 @@
+import random
+
+subjects=["Sharukh Khan ","A Fish ","Messi ","Balen shah ","A group of monkey ","Isospeed "]
+actions=["launch ","cancels ","dance with ", "eats ", "declear war on ","Play Scissor paper rock "]
+place_or_things=["a plate of samosa","in traffic","at meet-Galla","Biryani","Railway station","at FIFA world cup"]
+
+while True:
+    subject=random.choice(subjects)
+    action=random.choice(actions)
+    place_or_thing=random.choice(place_or_things)
+
+    Headline=f"Breaking News:{subject}{action}{place_or_thing}" 
+    print("\n"+ Headline)
+
+    user=input("\nDo you want to genetate another headline?(yes/no): ").strip().lower()
+    if user=="no":
+        break
+
+print("\nThanks for using Fake News Headline Generator...")
+print("goodBye.....")
+print("visit again.....")
+
+
+#store this in a txt file.
